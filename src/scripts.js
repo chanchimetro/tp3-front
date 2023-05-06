4f4a9551a27b9f4322acc4dd82fe383cd0d147d4
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-05-06 09:20:34 -0300

    finished buscarPizzas

--- src/scripts.js
+++ src/scripts.js
@@ -1,26 +1,33 @@
-import axios from 'axios'
-import { createElement } from 'react';
+import axios from "axios";
+import { createElement } from "react";
 
-let listaPizzas = null
+let listaPizzas = null;
 export class scripts {
-    static traerPizzas() {
-        axios.get('http://localhost:3001/pizza')
-            .then(function (response) {
-                console.log(response);
-                listaPizzas = response.data;
-                let ul = document.getElementById("lista");
-                ul.innerHTML = "";
-                listaPizzas.forEach(element => {
-                    let li = document.createElement("li");
-                    li.innerHTML = element.Nombre;
-                    ul.appendChild(li);
-                });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-    static buscarPizzas() {
-        //mapPizzas = listaPizzas.filter(i => i.nombre.contains)
-    }
+  static traerPizzas() {
+    axios
+      .get("http://localhost:3001/pizza")
+      .then(function (response) {
+        console.log(response);
+        listaPizzas = response.data;
+        this.renderizarLista(listaPizzas);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  static buscarPizzas() {
+    let inputVal = document.getElementById("input").value;
+    console.log(inputVal);
+    let mapPizzas = listaPizzas.filter((i) => i.nombre.contains(inputVal));
+    this.renderizarLista(mapPizzas);
+}
+  static renderizarLista(listaPizzas) {
+    let ul = document.getElementById("lista");
+    ul.innerHTML = "";
+    listaPizzas.forEach((element) => {
+      let li = document.createElement("li");
+      li.innerHTML = element.Nombre;
+      ul.appendChild(li);
+    });
+  }
 }
